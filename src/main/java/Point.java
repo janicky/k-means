f589a0420bfaee5586bfd832c192780d5c92f5f0
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-13 00:21:32 +0200

    Refactor Vector to Point and add global error

---
+++ src/main/java/Point.java
@@ -0,0 +1,27 @@
+public class Point {
+
+    private double x;
+    private double y;
+    private Centroid centroid;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public Centroid getCentroid() {
+        return centroid;
+    }
+
+    public void setCentroid(Centroid centroid) {
+        this.centroid = centroid;
+    }
+}
