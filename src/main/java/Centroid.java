acef9fdfeb92b4a3fbb838f4f4208df840d32b6e
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-12 16:25:38 +0200

    Add centroid fields and constructor

--- src/main/java/Centroid.java
+++ src/main/java/Centroid.java
@@ -3,7 +3,14 @@ import java.util.List;
 
 public class Centroid {
 
+    private int x;
+    private int y;
     List<Point> points = new ArrayList<>();
+
+    public Centroid(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     public boolean addPoint(Point p) {
         return points.add(p);
