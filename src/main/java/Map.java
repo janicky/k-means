3bb878b320042af71f827530ad12d25ed75c283b
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-12 16:57:35 +0200

    Add points from file loader

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,9 +1,34 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Map {
 
     private List<Centroid> centroids = new ArrayList<>();
+    private List<Point> points = new ArrayList<>();
 
+    public Map() {
+        loadPoints("attract_small.txt");
+        System.out.println(points.toString());
+    }
 
+    public void loadPoints(final String file_name) {
+        try {
+            File file = new File(file_name);
+            Scanner sc = new Scanner(file);
+
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String [] values = line.split("\\,");
+                points.add(new Point(
+                        Double.parseDouble(values[0]),
+                        Double.parseDouble(values[1])
+                ));
+            }
+
+        } catch (Exception e) {
+
+        }
+    }
 }
