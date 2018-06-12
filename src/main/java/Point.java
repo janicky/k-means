a22b2eb851b84cb33902d7112d34b78346b0b782
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-12 16:27:37 +0200

    Add getters to Point

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -7,4 +7,12 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 }
