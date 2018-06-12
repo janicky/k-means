7c6c0e40e5f875ece09a7ce5508282ea6f2b9005
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-12 16:53:59 +0200

    Implements toString() method in Point class

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -15,4 +15,15 @@ public class Point {
     public double getY() {
         return y;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("x: ");
+        sb.append(x);
+        sb.append("y: ");
+        sb.append(y);
+
+        return sb.toString();
+    }
 }
