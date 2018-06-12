dc23080894a99e92200d8935b64f2207f63c84c2
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-12 17:37:31 +0200

    Add assignToCentroids() and createCentroids() method in Map

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -12,9 +12,32 @@ public class Map {
     public Map(int k) {
         this.k = k;
         loadPoints("attract_small.txt");
+        createCentroids();
+        assignToCentroids();
+        System.out.println(centroids.toString());
     }
 
-    public void loadPoints(final String file_name) {
+    private void createCentroids() {
+        for (int i = 0; i < k; i++) {
+            centroids.add(new Centroid(-20.0, 20.0, -20, 20));
+        }
+    }
+
+    private void assignToCentroids() {
+        for (Point p : points) {
+            Centroid candidate = null;
+            double min_dist = Double.MAX_VALUE;
+            for (Centroid c : centroids) {
+                if (c.distanceTo(p) < min_dist) {
+                    min_dist = c.distanceTo(p);
+                    candidate = c;
+                }
+            }
+            candidate.addPoint(p);
+        }
+    }
+
+    private void loadPoints(final String file_name) {
         try {
             File file = new File(file_name);
             Scanner sc = new Scanner(file);
