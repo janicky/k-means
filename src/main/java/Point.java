c2ddab0e2b60a1fd3d06aa4a8bb32f9cb5b92dbd
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-12 16:57:17 +0200

    Change toString() method in Point

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -19,10 +19,10 @@ public class Point {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("x: ");
-        sb.append(x);
+        sb.append("[x: ");
+        sb.append(x + ", ");
         sb.append("y: ");
-        sb.append(y);
+        sb.append(y + "]");
 
         return sb.toString();
     }
